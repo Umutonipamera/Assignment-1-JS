915c108789a470d3649ef3ccda56cc4c7f0ef779
Umutonipamera <umutonipamera@gmail.com>
2021-03-11 12:02:58 +0300

    Assignment1 JS

---
+++ Assignment.js
@@ -0,0 +1,7 @@
+function Fruits(){
+    var fruit_list = ["Mangoes","Avocados","Oranges","Bananas","Apples"]
+    for (var i=0; i<fruit_list.length; i++){
+        console.log(fruit_list[i]);
+    }
+}
+Fruits();
